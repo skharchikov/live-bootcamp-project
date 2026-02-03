6f6df7bfe393b69fc37e389bfe6d4efe6c1bcc5c
Sergei Kharchikov <callmestech@gmail.com>
2026-02-03 01:58:57 +0000

    [s3][t1] Implement login route error cases

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,6 +1,46 @@
+use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
+use axum::Json;
+use serde::{Deserialize, Serialize};
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::app_state::AppState;
+use crate::{AuthAPIError, Email, Password, UserStoreError};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+impl LoginRequest {
+    pub fn new(email: String, password: String) -> Self {
+        Self { email, password }
+    }
+}
+
+pub async fn login(
+    State(state): State<AppState>,
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    let user_store = state.user_store.read().await;
+    let email = Email::parse(&request.email).map_err(|_| AuthAPIError::InvalidCredentials)?;
+    let password =
+        Password::parse(&request.password).map_err(|_| AuthAPIError::InvalidCredentials)?;
+
+    user_store
+        .validate_user(&email, &password)
+        .await
+        .map_err(|e| match e {
+            UserStoreError::UserNotFound | UserStoreError::InvalidCredentials => {
+                AuthAPIError::IncorrectCredentials
+            }
+            _ => AuthAPIError::UnexpectedError,
+        })?;
+
+    user_store.get_user(&email).await.map_err(|e| match e {
+        UserStoreError::UserNotFound => AuthAPIError::IncorrectCredentials,
+        _ => AuthAPIError::UnexpectedError,
+    })?;
+    Ok(StatusCode::OK.into_response())
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,17 +1,65 @@
-use crate::helpers::{LoginBody, TestApp};
-use fake::{
-    faker::internet::en::{Password, SafeEmail},
-    Fake,
-};
+use crate::helpers::TestApp;
+use auth_service::ErrorResponse;
+use fake::{faker::internet::en::SafeEmail, Fake};
+use serde_json::json;
 
 #[tokio::test]
-async fn login_returns_200_when_valid_data_provided() {
+async fn should_return_422_if_malformed_credentials() {
+    let app = TestApp::new().await;
+    let input = json!({
+        "email": "not-an-email"
+    });
+    let response = app.post_impl("/login", &input).await;
+    assert_eq!(response.status().as_u16(), 422);
+}
+
+#[tokio::test]
+async fn should_return_400_if_incorrect_credentials() {
+    let app = TestApp::new().await;
+    let input = json!({
+        "email": "not-an-email",
+        "password": "12345678"
+    });
+    let response = app.post_impl("/login", &input).await;
+    assert_eq!(response.status().as_u16(), 400);
+
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "Invalid credentials".to_owned()
+    );
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_input() {
     let app = TestApp::new().await;
     let email: String = SafeEmail().fake();
-    let password: String = Password(8..12).fake();
-    let login_body = LoginBody::new(email, password);
 
-    let response = app.login(login_body).await;
+    let input = serde_json::json!({
+        "email": email,
+        "password": "ValidPass1!",
+        "requires2FA": true
+    });
 
-    assert_eq!(response.status().as_u16(), 200);
+    let response = app.post_impl("/signup", &input).await;
+    assert_eq!(response.status().as_u16(), 201);
+
+    let input = json!({
+        "email": email,
+        "password": "InvalidPass1!"
+    });
+
+    let response = app.post_impl("/login", &input).await;
+    assert_eq!(response.status().as_u16(), 401);
+    assert_eq!(
+        response
+            .json::<ErrorResponse>()
+            .await
+            .expect("Could not deserialize response body to ErrorResponse")
+            .error,
+        "Incorrect credentials".to_owned()
+    )
 }
